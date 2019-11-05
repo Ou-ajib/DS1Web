fdc4e1128b3992697223a10e3ae7a38f17261c9e
Client11@EST.LAN <Client11@EST.LAN>
2019-11-05 14:55:16 +0000

    Question 3

--- code.js
+++ code.js
@@ -5,7 +5,9 @@ var temp_scores = document.getElementsByClassName("score-joueur-courent");
 var joueur0_panel = document.querySelector(".panel-joueur-0");
 var joueur1_panel = document.querySelector(".panel-joueur-1");
 var de = document.querySelector(".de");
-var btn = document.querySelector(".btn-lancer");
+var btn_launcer = document.querySelector(".btn-lancer");
+var btn_passer = document.querySelector(".btn-passe");
+var btn_nouveau = document.querySelector(".btn-new");
 
 function init(){
     scores[0].innerHTML = "0";
@@ -24,21 +26,34 @@ function changePlayer(){
     joueur1_panel.classList.toggle("actif");
 }
 
+function activeUserIndex(){
+    if(joueur0_panel.classList.contains('actif')) 
+        return 0;
+    return 1;
+}
+
 function launcer(){
     var number =  Math.floor((Math.random() * 6) + 1);
     de.setAttribute("src", "de-" +number+ ".png");
     de.style.display = "";
-    if(number != 1){
-        if(joueur0_panel.classList.contains('actif')) 
-            temp_scores[0].innerHTML = parseInt(temp_scores[0].innerHTML) + number;
-        else 
-            temp_scores[1].innerHTML = parseInt(temp_scores[1].innerHTML) + number;
-    } else {
+    if(number != 1)
+            temp_scores[activeUserIndex()].innerHTML = parseInt(temp_scores[0].innerHTML) + number;
+    else {
+        temp_scores[activeUserIndex()].innerHTML = 0;
         changePlayer();
     }
 }
 
-btn.addEventListener('click', launcer);
+function joueurSuivant(){
+    scores[activeUserIndex()].innerHTML = parseInt(scores[activeUserIndex()].innerHTML) + parseInt(temp_scores[activeUserIndex()].innerHTML);
+    temp_scores[activeUserIndex()].innerHTML = 0;
+    changePlayer();
+    de.style.display = "none";
+
+}
+
+btn_launcer.addEventListener('click', launcer);
+btn_passer.addEventListener('click', joueurSuivant);
 
 
 init();
