4cdd12e833008af802e5428fb4fa18e1aa77bd3f
Client11@EST.LAN <Client11@EST.LAN>
2019-11-05 14:40:49 +0000

    Question 2

--- code.js
+++ code.js
@@ -5,6 +5,7 @@ var temp_scores = document.getElementsByClassName("score-joueur-courent");
 var joueur0_panel = document.querySelector(".panel-joueur-0");
 var joueur1_panel = document.querySelector(".panel-joueur-1");
 var de = document.querySelector(".de");
+var btn = document.querySelector(".btn-lancer");
 
 function init(){
     scores[0].innerHTML = "0";
@@ -22,6 +23,22 @@ function changePlayer(){
     joueur0_panel.classList.toggle("actif");
     joueur1_panel.classList.toggle("actif");
 }
+
+function launcer(){
+    var number =  Math.floor((Math.random() * 6) + 1);
+    de.setAttribute("src", "de-" +number+ ".png");
+    de.style.display = "";
+    if(number != 1){
+        if(joueur0_panel.classList.contains('actif')) 
+            temp_scores[0].innerHTML = parseInt(temp_scores[0].innerHTML) + number;
+        else 
+            temp_scores[1].innerHTML = parseInt(temp_scores[1].innerHTML) + number;
+    } else {
+        changePlayer();
+    }
+}
+
+btn.addEventListener('click', launcer);
 
 
 init();
